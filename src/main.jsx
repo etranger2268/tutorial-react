90196311d59fe00786c7cb3b6d8ae01ec439fdcc
etranger2268 <shun.5.2268@gmail.com>
2025-12-18 00:00:48 +0900

    develop-01

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.jsx';
+import Square from './Square.jsx';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 root.render(
   <StrictMode>
-    <App />
+    <Square />
   </StrictMode>,
 );
