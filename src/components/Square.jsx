e14dec95ad23992e2d6c5e92ace3dc963bbb2b0c
etranger2268 <shun.5.2268@gmail.com>
2025-12-19 21:02:57 +0900

    develop-04

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,6 +1,16 @@
-export default function Square({value}) {
+import { useState } from 'react';
+
+export default function Square() {
+  const [value, setValue] = useState(null);
+
+  const handleClick = () => setValue('X');
+
   return (
-    <button className="bg-white border float-left text-2xl font-bold leading-8.5 h-8.5 mr-[-1]px mt-[-1]px p-0 text-center w-8.5">
+    <button
+      type="button"
+      onClick={handleClick}
+      className="bg-white border float-left text-2xl font-bold leading-8.5 h-8.5 mr-[-1]px mt-[-1]px p-0 text-center w-8.5"
+    >
       {value}
     </button>
   );
