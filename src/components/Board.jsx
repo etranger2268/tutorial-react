92cc501d4c9a6e720fedb09ea2ef1dbca5dfd768
etranger2268 <shun.5.2268@gmail.com>
2025-12-19 21:17:00 +0900

    develop-06

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -4,22 +4,25 @@ import Square from './Square';
 export default function Board() {
   const [squares, setSquares] = useState([...Array(9)].map(() => null));
 
+  const onSquareClick = (index) =>
+    setSquares((prev) => prev.map((val, i) => (i === index ? 'X' : val)));
+
   return (
     <div className="m-5 p-0">
       <div className="flex">
-        <Square value={squares[0]} />
-        <Square value={squares[1]} />
-        <Square value={squares[2]} />
+        <Square value={squares[0]} onSquareClick={() => onSquareClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => onSquareClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => onSquareClick(2)} />
       </div>
       <div className="flex">
-        <Square value={squares[3]} />
-        <Square value={squares[4]} />
-        <Square value={squares[5]} />
+        <Square value={squares[3]} onSquareClick={() => onSquareClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => onSquareClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => onSquareClick(5)} />
       </div>
       <div className="flex">
-        <Square value={squares[6]} />
-        <Square value={squares[7]} />
-        <Square value={squares[8]} />
+        <Square value={squares[6]} onSquareClick={() => onSquareClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => onSquareClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => onSquareClick(8)} />
       </div>
     </div>
   );
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,8 +1,9 @@
 
-export default function Square({ value }) {
+export default function Square({ value, onSquareClick }) {
   return (
     <button
       type="button"
+      onClick={onSquareClick}
       className="bg-white border float-left text-2xl font-bold leading-8.5 h-8.5 mr-[-1]px mt-[-1]px p-0 text-center w-8.5"
     >
       {value}
