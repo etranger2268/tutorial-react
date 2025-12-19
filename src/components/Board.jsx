22ba8ff402bd4386cbb042ec6789a5b3835fc5a6
etranger2268 <shun.5.2268@gmail.com>
2025-12-19 21:10:31 +0900

    develop-05

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,22 +1,25 @@
+import { useState } from 'react';
 import Square from './Square';
 
 export default function Board() {
+  const [squares, setSquares] = useState([...Array(9)].map(() => null));
+
   return (
     <div className="m-5 p-0">
       <div className="flex">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
       </div>
       <div className="flex">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
       </div>
       <div className="flex">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
       </div>
     </div>
   );
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,14 +1,8 @@
-import { useState } from 'react';
 
-export default function Square() {
-  const [value, setValue] = useState(null);
-
-  const handleClick = () => setValue('X');
-
+export default function Square({ value }) {
   return (
     <button
       type="button"
-      onClick={handleClick}
       className="bg-white border float-left text-2xl font-bold leading-8.5 h-8.5 mr-[-1]px mt-[-1]px p-0 text-center w-8.5"
     >
       {value}
