e3d5110231af1eb155fb6455b99641980a753f91
etranger2268 <shun.5.2268@gmail.com>
2025-12-20 00:02:41 +0900

    develop-12

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -5,14 +5,20 @@ import Moves from './Moves';
 export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([[...Array(9)].map(() => null)]);
-  const currentSquares = history.at(-1);
+  const [currentMove, setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove];
 
   const handlePlay = (nextSquares) => {
-    setHistory((prev) => [...prev, nextSquares]);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
     setXIsNext((prev) => !prev);
   };
 
-  const jumpTo = (index) => {};
+  const jumpTo = (nextMove) => {
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 === 0);
+  };
 
   return (
     <div className="flex flex-row space-x-8">
